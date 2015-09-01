8b45c5377c5de6c20345c01fcd6b8c06e2b9cc68
Thomas <thomms27@hotmail.com>
2015-09-01 16:32:16 +1000

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AGL_Test
+{
+    class Person
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public List<Pet> pets { get; set; }
+    }
+}
