7f633104976e67eeab97284128f1ad56be66dbde
Thomas <thomms27@hotmail.com>
2015-09-01 16:32:53 +1000

    Create Pet.cs

---
+++ Pet.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace AGL_Test
+{
+    class Pet
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+}
